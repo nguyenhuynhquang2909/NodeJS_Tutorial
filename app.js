e8d89cef974d33c33ae987a1d3305cedc85fa7b2
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2023-08-07 13:59:29 +0700

    Finishing ejs

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const bodyParser = require('body-parser');
 const expressHbs = require('express-handlebars');
 const app = express();
 
-app.engine('handlebars', expressHbs.engine({extname: 'handlebars', defaultLayout: false, layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}))
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 app.set('views', 'views')
 
 const adminData = require('./routes/admin');
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, docTitle: 'shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true});
+  res.render('shop', {prods: products, pageTitle: 'shop', path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true});
 });
 
 module.exports = router;

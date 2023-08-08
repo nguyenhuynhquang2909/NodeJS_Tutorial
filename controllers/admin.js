00c0a918603bc06bc4a23571e98a63325beac571
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2023-08-08 17:31:34 +0700

    Finishing storing product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,6 @@
 const Product = require('../models/product');
-
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
       pageTitle: 'Add Product', 
       path: '/admin/add-product', 
       formCSS: true, 
@@ -9,19 +8,20 @@ exports.getAddProduct = (req, res, next) => {
       activeAddProduct: true});
   }
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl
+    const price = req.body.price
+    const description = req.body.description
+    const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
   }
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products => {
-        res.render('shop', {
+        res.render('admin/products', {
             prods: products, 
             pageTitle: 'shop', 
             path: '/', 
-            hasProducts: products.length > 0, 
-            activeShop: true, 
-            productCSS: true})
-    }))
-   ;
-  }
+            })
+    }));
+}

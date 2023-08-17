dd95e0a32ea4b2329fc42cb3471f92a6fa92f96a
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2023-08-17 10:49:30 +0700

    Finishing working with sequelize

--- util/database.js
+++ util/database.js
@@ -1,8 +1,7 @@
-const mysql = require('mysql2');
-const pool = mysql.createPool({
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'Bebong123', {
+    dialect: 'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Bebong123'
 });
-module.exports = pool.promise()
+module.exports = sequelize;
